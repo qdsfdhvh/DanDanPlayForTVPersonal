4d954bf78df552098c118c7f967944d7a64f3658
Seiko <605590140@qq.com>
2020-04-08 16:27:00 +0800

    feature/vlc:player路由转到vlc。

--- module-player/src/main/java/com/seiko/player/ui/VlcMediaActivity.kt
+++ module-player/src/main/java/com/seiko/player/ui/VlcMediaActivity.kt
@@ -1,32 +1,17 @@
 package com.seiko.player.ui
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.seiko.common.router.Routes
 
-@Route(path = Routes.Player.PATH)
-class StartActivity : AppCompatActivity() {
-
+@Route(path = Routes.Player.PATH_MEDIA)
+class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkIntent()
-    }
-
-    private fun checkIntent() {
-        val openIntent = intent
         val intent = Intent(applicationContext, org.videolan.vlc.StartActivity::class.java)
-
-        val videoUri: Uri? = openIntent?.getParcelableExtra(Routes.Player.ARGS_VIDEO_URI)
-        if (videoUri != null) {
-            intent.action = Intent.ACTION_VIEW
-            intent.setDataAndType(videoUri, "video/*")
-        }
-
         startActivity(intent)
         finish()
     }
-
 }
