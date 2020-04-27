5fa1b5b18eb88d6a8e67093c4f2c0c21e1068ca8
Seiko <605590140@qq.com>
2020-04-27 10:03:00 +0800

    fixbug:修复DispatchKeyEventDispatcher按键分配问题。

--- common/src/main/java/androidx/activity/DispatchKeyEventDispatcher.kt
+++ common/src/main/java/androidx/activity/DispatchKeyEventDispatcher.kt
@@ -89,10 +89,10 @@ class DispatchKeyEventDispatcher(
 
         override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
             when(event) {
-                Lifecycle.Event.ON_START -> {
+                Lifecycle.Event.ON_RESUME -> {
                     currentCancellable = addCancellableCallback(dispatchKeyEventCallback)
                 }
-                Lifecycle.Event.ON_STOP -> {
+                Lifecycle.Event.ON_PAUSE -> {
                     currentCancellable?.cancel()
                 }
                 Lifecycle.Event.ON_DESTROY -> {
