735f401a62afb4de0865c56c9cfe74d4fdf19384
seiko <605590140@qq.com>
2020-08-01 10:22:45 +0800

    移除okhttp cache

--- common/src/main/java/com/seiko/common/di/NetworkModule.kt
+++ common/src/main/java/com/seiko/common/di/NetworkModule.kt
@@ -1,30 +1,19 @@
 package com.seiko.common.di
 
-import android.content.Context
 import com.squareup.moshi.Moshi
-import okhttp3.Cache
 import okhttp3.OkHttpClient
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Converter
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.io.File
 import java.util.concurrent.TimeUnit
 
 internal val networkModule = module {
-    single { createCache(androidContext()) }
-    single { createSingleHttpClient(get()) }
+    single { createSingleHttpClient() }
     single { createConverterFactory(get()) }
 }
 
-private fun createCache(context: Context): Cache {
-    val file = File(context.cacheDir, "HttpResponseCache")
-    return Cache(file, 50 * 1024 * 1024)
-}
-
-private fun createSingleHttpClient(cache: Cache): OkHttpClient {
+private fun createSingleHttpClient(): OkHttpClient {
     return OkHttpClient.Builder()
-        .cache(cache)
         .connectTimeout(15, TimeUnit.SECONDS)
         .readTimeout(10, TimeUnit.SECONDS)
         .writeTimeout(10, TimeUnit.SECONDS)
