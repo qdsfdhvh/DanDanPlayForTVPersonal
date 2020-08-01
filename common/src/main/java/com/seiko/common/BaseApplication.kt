514233a946ec16a2957a7669f1aace8b7eb03469
seiko <605590140@qq.com>
2020-08-01 14:22:03 +0800

    创建BaseApplication

---
+++ common/src/main/java/com/seiko/common/BaseApplication.kt
@@ -0,0 +1,88 @@
+package com.seiko.common
+
+import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import android.os.StrictMode
+import androidx.multidex.MultiDex
+import com.alibaba.android.arouter.launcher.ARouter
+import com.seiko.common.app.AppDelegate
+import com.seiko.common.app.AppSetupDelegate
+import com.seiko.common.util.timber.NanoDebugTree
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+abstract class BaseApplication : Application(), AppDelegate by AppSetupDelegate() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(base)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+        setupARouter()
+        // 注解
+        startKoin {
+            androidContext(this@BaseApplication)
+        }
+        setupApplication()
+        setupStrictModel()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        configurationChanged(newConfig)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        clearOnLowMemory()
+    }
+}
+
+
+/**
+ * 初始化日志
+ */
+private fun setupTimber() {
+    if (BuildConfig.DEBUG) {
+        Timber.plant(NanoDebugTree())
+    }
+}
+
+/**
+ * 初始化路由
+ */
+private fun Application.setupARouter() {
+    if (BuildConfig.DEBUG) {
+        // 打印日志
+        ARouter.openLog()
+        //开启调试模式
+        ARouter.openDebug()
+    }
+    ARouter.init(this)
+}
+
+/**
+ * 开启严格模式
+ */
+private fun setupStrictModel() {
+    if (BuildConfig.DEBUG) {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+            .build())
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+            .detectLeakedSqlLiteObjects()
+            .detectActivityLeaks()
+//            .detectLeakedClosableObjects()
+            .penaltyLog()
+            .penaltyDeath()
+            .build())
+    }
+}
--- component/tv/src/main/debug/java/com/seiko/tv/App.kt
+++ component/tv/src/main/debug/java/com/seiko/tv/App.kt
@@ -1,55 +1,5 @@
 package com.seiko.tv
 
-import android.app.Application
-import android.content.Context
-import android.content.res.Configuration
-import androidx.multidex.MultiDex
-import com.alibaba.android.arouter.launcher.ARouter
-import com.seiko.common.app.AppDelegate
-import com.seiko.common.app.AppSetupDelegate
-import com.seiko.common.util.timber.NanoDebugTree
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
-import timber.log.Timber
+import com.seiko.common.BaseApplication
 
-class App : Application(), AppDelegate by AppSetupDelegate() {
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        // 日志
-        if (BuildConfig.DEBUG) {
-            Timber.plant(NanoDebugTree())
-        }
-
-        // 路由
-        if (BuildConfig.DEBUG) {
-            // 打印日志
-            ARouter.openLog()
-            //开启调试模式
-            ARouter.openDebug()
-        }
-        ARouter.init(this)
-
-        // 注解
-        startKoin {
-            androidContext(this@App)
-        }
-
-        setupApplication()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        configurationChanged(newConfig)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        clearOnLowMemory()
-    }
-}
+class App : BaseApplication()
