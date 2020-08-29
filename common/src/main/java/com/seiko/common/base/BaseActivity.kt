ccd6b9074349d891337970b9ad6ca5411f3e33df
seiko <605590140@qq.com>
2020-08-29 15:37:11 +0800

    fixbug:修复部分内存泄漏

--- common/src/main/java/com/seiko/common/base/BaseActivity.kt
+++ common/src/main/java/com/seiko/common/base/BaseActivity.kt
@@ -1,7 +1,11 @@
 package com.seiko.common.base
 
+import android.os.Build
+import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.FragmentActivity
+import com.seiko.common.util.fix.FixActivityFromTransitionManager
+import com.seiko.common.util.fix.LeakFreeSupportSharedElementCallback
 
 abstract class BaseActivity : FragmentActivity {
 
@@ -9,4 +13,14 @@ abstract class BaseActivity : FragmentActivity {
 
     constructor(@LayoutRes contentLayoutId: Int) : super(contentLayoutId)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        FixActivityFromTransitionManager.removeActivityFromTransitionManager(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setEnterSharedElementCallback(LeakFreeSupportSharedElementCallback())
+            setExitSharedElementCallback(LeakFreeSupportSharedElementCallback())
+        }
+    }
 }
