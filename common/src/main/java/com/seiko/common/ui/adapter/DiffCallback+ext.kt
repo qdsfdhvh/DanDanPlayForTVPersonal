6246c9c22d45b958e1619b0e6544dfed2cb0d63c
seiko <605590140@qq.com>
2020-12-29 20:07:47 +0800

    DiffCallback中 T : Any

--- common/src/main/java/com/seiko/common/ui/adapter/DiffCallback+ext.kt
+++ common/src/main/java/com/seiko/common/ui/adapter/DiffCallback+ext.kt
@@ -6,18 +6,28 @@ import androidx.recyclerview.widget.DiffUtil
 /**
  * 将[DiffCallback]转为[DiffUtil.ItemCallback]
  */
-internal fun <T> DiffCallback<T>.toItemCallback(): DiffUtil.ItemCallback<T> {
+internal fun <T : Any> DiffCallback<T>.toItemCallback(): DiffUtil.ItemCallback<T> {
     return object : DiffUtil.ItemCallback<T>() {
-        override fun areItemsTheSame(oldItem: T, newItem: T) = this@toItemCallback.areItemsTheSame(oldItem, newItem)
-        override fun areContentsTheSame(oldItem: T, newItem: T) = this@toItemCallback.areContentsTheSame(oldItem, newItem)
-        override fun getChangePayload(oldItem: T, newItem: T): Any? = this@toItemCallback.getChangePayload(oldItem, newItem)
+        override fun areItemsTheSame(oldItem: T, newItem: T) =
+            this@toItemCallback.areItemsTheSame(oldItem, newItem)
+
+        override fun areContentsTheSame(oldItem: T, newItem: T) =
+            this@toItemCallback.areContentsTheSame(oldItem, newItem)
+
+        override fun getChangePayload(oldItem: T, newItem: T): Any? =
+            this@toItemCallback.getChangePayload(oldItem, newItem)
     }
 }
 
-internal fun <T> DiffUtil.ItemCallback<T>.toDiffCallback(): DiffCallback<T> {
+internal fun <T : Any> DiffUtil.ItemCallback<T>.toDiffCallback(): DiffCallback<T> {
     return object : DiffCallback<T>() {
-        override fun areItemsTheSame(oldItem: T, newItem: T) = this@toDiffCallback.areItemsTheSame(oldItem, newItem)
-        override fun areContentsTheSame(oldItem: T, newItem: T) = this@toDiffCallback.areContentsTheSame(oldItem, newItem)
-        override fun getChangePayload(oldItem: T, newItem: T): Any? = this@toDiffCallback.getChangePayload(oldItem, newItem)
+        override fun areItemsTheSame(oldItem: T, newItem: T) =
+            this@toDiffCallback.areItemsTheSame(oldItem, newItem)
+
+        override fun areContentsTheSame(oldItem: T, newItem: T) =
+            this@toDiffCallback.areContentsTheSame(oldItem, newItem)
+
+        override fun getChangePayload(oldItem: T, newItem: T): Any? =
+            this@toDiffCallback.getChangePayload(oldItem, newItem)
     }
 }
