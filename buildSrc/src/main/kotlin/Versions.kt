ee381225e15e0ca22b4e0d8710d72d6b7425fa4d
seiko <605590140@qq.com>
2021-05-18 21:31:05 +0800

    retrofit to 2.9.0 && okhttp to 4.9.1 && moshi to 1.12.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -24,9 +24,9 @@ object Versions {
 
     const val mmkv = "1.1.1" // https://github.com/Tencent/MMKV
 
-    const val retrofit = "2.8.1" // https://github.com/square/retrofit
-    const val okhttp = "4.6.0" // https://github.com/square/okhttp
-    const val moshi = "1.9.2" // https://github.com/square/moshi
+    const val retrofit = "2.9.0" // https://github.com/square/retrofit
+    const val okhttp = "4.9.1"   // https://github.com/square/okhttp
+    const val moshi = "1.12.0"   // https://github.com/square/moshi
 
     const val dagger = "2.35.1"  // https://github.com/google/dagger
     const val hilt = "1.0.0"     // https://maven.google.com/web/index.html?q=hilt#androidx.hilt:hilt-common
